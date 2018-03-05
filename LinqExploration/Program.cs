fd4d5c9f2c8fa3f667b84f0db2cf949332eb061c
Sean <swbrooks87@gmail.com>
2018-03-05 17:40:25 -0500

    Clean up

--- LinqExploration/Program.cs
+++ LinqExploration/Program.cs
@@ -8,68 +8,6 @@ namespace LinqExploration
     {
         static void Main(string[] args)
         {
-            string[] names = {
-                "Burke",
-                "Connor",
-                "Frank",
-                "Everett",
-                "Albert",
-                "George",
-                "Harris",
-                "David"
-            };
-
-            // The local variable query is initialized with a query expression.
-            IEnumerable<string> query = from s in names
-                                  where s.Length == 5 
-                                  orderby s
-                                  select s.ToUpper();
-
-            /*
-                This form of query is called a method-based query. 
-                The arguments to the Where, OrderBy, and Select operators are called lambda expressions,
-                which are fragments of code much like delegates. They allow the standard query operators 
-                to be defined individually as methods and strung together using dot notation. 
-                Together, these methods form the basis for an extensible query language.
-             */
-            IEnumerable<string> query2 = names.Where(s => s.Length == 5)
-                                             .OrderBy(s => s)
-                                             .Select(s => s.ToUpper());
-
-            foreach (string item in query2)
-                Console.WriteLine(item);
-
-            // Lamba Expressions and Expression Trees
-            Func<string, bool> filter = s => s.Length != 5;
-            Func<string, string> extract = s => s;
-            Func<string, string> project = s => s.ToLower();
-
-            IEnumerable<string> query3 = names.Where(filter)
-                                             .OrderBy(extract)
-                                             .Select(project);
-
-            foreach (string item in query3)
-                Console.WriteLine(item);
-
-            /* 
-                create table People (
-                    Name nvarchar(32) primary key not null,
-                    Age int not null,
-                    CanCode bit not null
-                );
-            */
-
-            // The CLR equivalent (Linq to SQL)
-            //[Table(Name="People")]
-            //public class Person {
-            //    [Column(DbType = "nvarchar(32) not null", Id = true)]
-            //    public string Name;
-            //    [Column]
-            //    public int Age;
-            //    [Column]
-            //    public bool CanCode;
-            //}
-
             // Attempt to connect to MySQL DB
             var db = new DBConnect();
 
